3ff723143c603b1eda297096305e3599878d6ee1
Khalil <khalil.oumimoun@gmail.com>
2019-04-07 15:10:21 -0500

    Day6: 0407 - Problem 14

---
+++ JavaScript/014_CollatzSequence.js
@@ -0,0 +1,50 @@
+
+var masterArray = [];
+function Collatz_Sequence(startNumber){
+    var sequenceList = [startNumber];
+    var next = 0;
+    var next_startNumber = startNumber + 1;
+    while (next != 1){
+       if(startNumber % 2 == 0){
+           startNumber = startNumber/2;
+           sequenceList.push(startNumber);
+           next = startNumber;
+       }else{
+           startNumber = 3*startNumber + 1;
+           sequenceList.push(startNumber);
+           next = startNumber;
+       }
+    }//if (next_startNumber < 10000){
+       // Collatz_Sequence(next_startNumber);
+   // }   
+    masterArray.push(sequenceList);  
+    return masterArray;
+    return masterArray.reduce(function(a,i,ii){
+        if (ii === 1){
+          return a
+        };
+        if (i.length > a.length){
+          return i
+        }
+        return a
+      });
+    
+}
+for (i = 13; i < 1000000; i++){
+    Collatz_Sequence(i);
+}
+
+console.log(masterArray.reduce(function(a,i,ii){
+    if (ii === 1){
+      return a
+    };
+    if (i.length > a.length){
+      return i
+    }
+    return a
+  })[0]);
+
+
+
+
+
